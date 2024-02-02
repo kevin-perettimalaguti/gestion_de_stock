5c4636dc687364d4678433276d361688baf94c23
kevin.perettimalaguti <kevin.peretti-malaguti@laplateforme.io>
2024-02-02 21:10:11 +0100

    push final pour la consigne principal

--- class_py/interface.py
+++ class_py/interface.py
@@ -1,182 +1,211 @@
-from tkinter import *
+import tkinter as tk
 from tkinter import ttk, messagebox
+from class_py.database import Database
 from class_py.product import Product
 from class_py.category import Category
-from class_py.database import Database
+
 class Interface:
     def __init__(self):
         self.database = Database("localhost", "root", "1478", "store")
         self.product = Product()
         self.category = Category()
-        self.fenetre = Tk()
-        self.fenetre.geometry("900x500")
-        self.fenetre.title("Stockage store")
-        self.widgets_add()
-        self.widgets_modify()
-        self.widgets_delete()
-        self.widgets_display_database()
+        self.fenetre = tk.Tk()
+        self.fenetre.title("Storage stock")
+        self.insert_data_on_board()
+        self.configure_widgets()        
 
-    def widgets_add(self):
-        label_add_productt = Label(self.fenetre, text="Add product", font=("Arial 13"), width=16, height=1, padx=5, pady=1, relief="flat",
-                     anchor=NW)
-        label_add_productt.place(x=470, y=50)
+    def configure_widgets(self):
+        self.configure_product_management()
+        self.configure_category_management()
 
-        # Name
-        label_name_add = Label(self.fenetre, text="Name", font=("Arial 11"), width=10, height=1, padx=5, pady=1, relief="flat",
-                     anchor=NW)
-        label_name_add.place(x=400, y=100)
-        name_add = Entry(self.fenetre, width=10)
-        name_add.place(x=400, y=130)
+    def insert_data_on_board(self):
+        self.board = ttk.Treeview(self.fenetre)
+        self.board['columns'] = ('ID', 'Name', 'Description', 'Price', 'Quantity', 'Category', 'Category ID')
 
-        # Description
-        label_description_add = Label(self.fenetre, text="Description", font=("Arial 11"), width=10, height=1, padx=5, pady=1, relief="flat",
-                     anchor=NW)
-        label_description_add.place(x=490, y=100)
-        description_add = Text(self.fenetre, height=1, width=10)
-        description_add.place(x=490, y=130)
-
-        # Price
-        label_price_add = Label(self.fenetre, text="Price", font=("Arial 11"), width=10, height=1, padx=5, pady=1, relief="flat",
-                     anchor=NW)
-        label_price_add.place(x=600, y=100)
-        price_add = Entry(self.fenetre, width=10)
-        price_add.place(x=600, y=130) 
-
-        # Quantity
-        label_quantity_add = Label(self.fenetre, text="Quantity", font=("Arial 11"), width=10, height=1, padx=5, pady=1, relief="flat",
-                     anchor=NW)
-        label_quantity_add.place(x=700, y=100)
-        quantity_add = Entry(self.fenetre, width=10)
-        quantity_add.place(x=700, y=130)
-
-        # Category
-        label_category_add = Label(self.fenetre, text="Category", font=("Arial 11"), width=10, height=1, padx=5, pady=1, relief="flat",
-                     anchor=NW)
-        label_category_add.place(x=800, y=100)
-        category_add = ttk.Combobox(self.fenetre, width=5, font=("Arial 11"))
-        category_add.place(x=800, y=130)
-        return name_add, description_add, price_add, quantity_add, category_add
-        # bouton_add = Button(self.fenetre, text="Ajouter", command=None , width=6, font=("Arial", 11), height=1)
-        # bouton_add.place(x=630, y=45) 
-    
-    def add_product(self):
-        name = self.widgets_add[0]
-        description = self.widgets_add[1]
-        price = self.widgets_add[2]
-        quantity = self.widgets_add[3]
-        category = self.widgets_add[4]
+        self.configure_columns_and_headings()
         
-        if name is not None:
-            self.product.add_product(name, description, price, quantity, category)            
-        else:
-            messagebox.showerror(f"Error, product not add")
+        self.refresh_button = tk.Button(self.fenetre, text="Refresh", command=self.refresh_data)
+        self.refresh_button.pack(side=tk.RIGHT, anchor=tk.N)
 
-    def widgets_modify(self):
-        label_add_productt_modif = Label(self.fenetre, text="Modify product", font=("Arial 13"), width=16, height=1, padx=5, pady=1, relief="flat",
-                     anchor=NW)
-        label_add_productt_modif.place(x=470, y=200)
+        self.board.pack(fill=tk.BOTH, expand=True)
 
-        # Name
-        label_name_modif = Label(self.fenetre, text="Name", font=("Arial 11"), width=10, height=1, padx=5, pady=1, relief="flat",
-                     anchor=NW)
-        label_name_modif.place(x=400, y=250)
-        name_modif = Entry(self.fenetre, width=10)
-        name_modif.place(x=400, y=280)
+    def configure_columns_and_headings(self):
+        columns = [
+            ("#0", 0, tk.W, ""),
+            ("ID", 40, tk.CENTER, "ID"),
+            ("Name", 80, tk.CENTER, "Name"),
+            ("Description", 120, tk.CENTER, "Description"),
+            ("Price", 60, tk.CENTER, "Price"),
+            ("Quantity", 60, tk.CENTER, "Quantity"),
+            ("Category", 60, tk.CENTER, "Category"),
+            ("Category ID", 80, tk.CENTER, "Category ID")
+        ]
 
-        # Description
-        label_description_modif = Label(self.fenetre, text="Description", font=("Arial 11"), width=10, height=1, padx=5, pady=1, relief="flat",
-                     anchor=NW)
-        label_description_modif.place(x=490, y=250)
-        description_modif = Text(self.fenetre, height=1, width=10)
-        description_modif.place(x=490, y=280)
+        for col, width, anchor, heading in columns:
+            self.board.column(col, width=width, anchor=anchor)
+            self.board.heading(col, text=heading, anchor=anchor)
 
-        # Price
-        label_price_modif = Label(self.fenetre, text="Price", font=("Arial 11"), width=10, height=1, padx=5, pady=1, relief="flat",
-                     anchor=NW)
-        label_price_modif.place(x=600, y=250)
-        price_modif= Entry(self.fenetre, width=10)
-        price_modif.place(x=600, y=280) 
+    def configure_product_management(self):
+        data_frame = tk.Frame(self.fenetre)
+        data_frame.pack(fill=tk.BOTH, padx=10, pady=10)
 
-        # Quantity
-        label_quantity_modif = Label(self.fenetre, text="Quantity", font=("Arial 11"), width=10, height=1, padx=5, pady=1, relief="flat",
-                     anchor=NW)
-        label_quantity_modif.place(x=700, y=250)
-        quantity_modif= Entry(self.fenetre, width=10)
-        quantity_modif.place(x=700, y=280)
+        labels_entries = [
+            ("Name:", 0, 0),
+            ("Description:", 1, 0),
+            ("Price:", 2, 0),
+            ("Quantity:", 3, 0),
+            ("Category:", 4, 0)
+        ]
 
-        # Category
-        label_category_modif= Label(self.fenetre, text="Categorie", font=("Arial 11"), width=10, height=1, padx=5, pady=1, relief="flat",
-                     anchor=NW)
-        label_category_modif.place(x=800, y=250)
-        category_modif = ttk.Combobox(self.fenetre, width=5, font=("Arial 11"))
-        category_modif.place(x=800, y=280)
+        for label, row, col in labels_entries:
+            tk.Label(data_frame, text=label).grid(row=row, column=col, sticky=tk.W)
 
-        # bouton_modif = Button(self.fenetre, text="Valider", command=None, width=6, font=("Arial", 11), height=1)
-        # bouton_modif.place(x=640, y= 195) 
-        # command=product.add_product()
-        return name_modif, description_modif, price_modif, quantity_modif, category_modif   
+        self.name_entry = tk.Entry(data_frame)
+        self.description_entry = tk.Entry(data_frame)
+        self.price_entry = tk.Entry(data_frame)
+        self.quantity_entry = tk.Entry(data_frame)
+        self.category_combobox = ttk.Entry(data_frame)
 
-    def widgets_delete(self):
-        label_add_productt_add = Label(self.fenetre, text="Supprimer un produit", font=("Arial 13"), width=20, height=1, padx=5, pady=1, relief="flat",
-                     anchor=NW)
-        label_add_productt_add.place(x=470, y=340)
+        entries = [
+            (self.name_entry, 0, 1),
+            (self.description_entry, 1, 1),
+            (self.price_entry, 2, 1),
+            (self.quantity_entry, 3, 1),
+            (self.category_combobox, 4, 1)
+        ]
 
-        #id
-        id_product = ttk.Combobox(self.fenetre, width=5, font=("Arial 11"))
-        id_product.place(x=425, y= 380)
-        
-        # bouton_supp = Button(self.fenetre, text="Supprimer", command=None , width=8, font=("Arial", 11), height=1)
-        # bouton_supp.place(x=520, y= 375)
-        return id_product
-    
-    def insert_product_on_board(self):
+        for entry, row, col in entries:
+            entry.grid(row=row, column=col)
+
+        buttons = [
+            ("Add Product", self.add_product, 5, 0),
+            ("Modify Product", self.modify_product, 5, 1),
+            ("Delete Product", self.remove_product, 5, 2)
+        ]
+
+        for text, command, row, col in buttons:
+            tk.Button(data_frame, text=text, command=command).grid(row=row, column=col, pady=5)
+
+    def configure_category_management(self):
+        fill_management_data = tk.Frame(self.fenetre)
+        fill_management_data.pack(side=tk.RIGHT, padx=10, pady=10)
+
+        labels_entries = [
+            ("Category Name:", 0, 0),
+            ("Category ID (for modify/delete):", 1, 0)
+        ]
+
+        for label, row, col in labels_entries:
+            tk.Label(fill_management_data, text=label).grid(row=row, column=col, sticky=tk.W)
+
+        self.category_name_entry = tk.Entry(fill_management_data)
+        self.category_id_entry = tk.Entry(fill_management_data)
+
+        entries = [
+            (self.category_name_entry, 0, 1),
+            (self.category_id_entry, 1, 1)
+        ]
+
+        for entry, row, col in entries:
+            entry.grid(row=row, column=col)
+
+        buttons = [
+            ("Add Category", self.add_category, 2, 0),
+            ("Modify Category", self.modify_category, 2, 1),
+            ("Delete Category", self.remove_category, 2, 2)
+        ]
+
+        for text, command, row, col in buttons:
+            tk.Button(fill_management_data, text=text, command=command).grid(row=row, column=col, pady=5)
+
+    def populate_product_tree(self):
         for item in self.board.get_children():
             self.board.delete(item)
-        
-        products = self.product.display_all_product()
+
+        products = self.product.fetch_all_products()
         for product in products:
             product_id, name, description, price, quantity, category_id = product
 
             category_name = self.get_category_name_from_id(category_id)
 
             self.board.insert("", 'end', values=(product_id, name, description, price, quantity, category_name, category_id))
-            
+
+    def fill_category_combobox(self):
+        categories = self.category.get_all_categories()
+        category_names = [category[1] for category in categories]
+        self.category_combobox['values'] = category_names
+
+    def get_category_id_from_name(self, category_name):
+        categories = self.category.get_all_categories()
+        for category in categories:
+            if category[1] == category_name:
+                return category[0]
+
+    def add_product(self):
+        name = self.name_entry.get()
+        description = self.description_entry.get()
+        price = self.price_entry.get()
+        quantity = self.quantity_entry.get()
+        category_name = self.category_combobox.get()
+
+        id_category = self.get_category_id_from_name(category_name)
+
+        if id_category is not None:
+            self.product.add_product(name, description, price, quantity, id_category)
+            self.populate_product_tree()
+        else:
+            messagebox.showerror("Error", "Category not found")
+
+    def remove_product(self):
+        selected_item = self.board.selection()
+        if selected_item:
+            product_id = self.board.item(selected_item)['values'][0]
+            self.product.remove_product(product_id)
+            self.populate_product_tree()
+        else:
+            pass
+
+    def modify_product(self):
+        selected_item = self.board.selection()
+        if selected_item:
+            product_id = self.board.item(selected_item)['values'][0]
+
+            new_name = self.name_entry.get()
+            new_description = self.description_entry.get()
+            new_price = self.price_entry.get()
+            new_quantity = self.quantity_entry.get()
+            new_id_category = self.category_combobox.get()
+
+            self.product.modify_product(product_id, new_name, new_description, new_price, new_quantity, new_id_category)
+            self.populate_product_tree()
+        else:
+            pass
+
+    def add_category(self):
+        category_name = self.category_name_entry.get()
+        self.category.add_category(category_name)
+        self.fill_category_combobox()
+
+    def remove_category(self):
+        category_id = self.category_id_entry.get()
+        self.category.delete_category(category_id)
+        self.fill_category_combobox()
+
+    def modify_category(self):
+        category_id = self.category_id_entry.get()
+        new_name = self.category_name_entry.get()
+        self.category.modify_category(category_id, new_name)
+        self.fill_category_combobox()
+
+    def refresh_data(self):
+        self.populate_product_tree()
+
     def get_category_name_from_id(self, category_id):
         categories = self.category.get_all_categories()
         for category in categories:
             if category[0] == category_id:
-                return category[1]         
- 
+                return category[1]
 
-    def widgets_display_database(self):
-        self.board = ttk.Treeview(self.fenetre)
-        self.board['columns'] = ('ID', 'Name', 'Description', 'Price', 'Quantity', 'Category', 'Category ID')
-
-        self.board.column("#0", width=0,)
-        self.board.column("ID", width=40)
-        self.board.column('Name', width=80)
-        self.board.column("Description", width=120)
-        self.board.column("Price", width=60)
-        self.board.column("Quantity", width=60)
-        self.board.column("Category", width=60)
-        self.board.column("Category ID", width=80)
-
-        # Create Headings
-        self.board.heading("#0", text="", width=0)
-        self.board.heading("ID", text="ID")
-        self.board.heading("Name", text="Name")
-        self.board.heading("Description", text="Description")
-        self.board.heading("Price", text="Price")
-        self.board.heading("Quantity", text="Quantity")
-        self.board.heading("Category", text="Category")
-        self.board.heading("Category ID", text="Category ID")
-
-        self.board.pack(expand=True)
-        
-    def refresh_data(self):
-        self.insert_product_on_board()
-    
     def running(self):
+        self.fenetre.geometry("700x500")
         self.fenetre.mainloop()
-
-
--- class_py/product.py
+++ class_py/product.py
@@ -5,48 +5,36 @@ class Product:
         self.database = Database("localhost", "root", "1478", "store")             
         
     def add_product(self, name, description, price, quantity, id_category):
-        sql = f"INSERT INTO product (name,description,price,quantity,id_category)" \
-            "VALUES" \
-                f"('{name}','{description}',{price},{quantity},{id_category});" 
+        sql = f"INSERT INTO product (name, description, price, quantity, id_category)" \
+              f" VALUES ('{name}', '{description}', {price}, {quantity}, {id_category});" 
         self.database.execute_sql(sql)        
         
     def remove_product(self, id_product):
         self.database.execute_sql(f"DELETE FROM product WHERE id = {id_product};")        
         
     def modify_product(self, id_product, new_name, new_description, new_price, new_quantity, new_id_category):
-        sql = "UPDATE product" \
-            f"SET" \
-            f"name = {new_name}," \
-            f"description = {new_description}," \
-            f"price = {new_price}," \
-            f"quantity = {new_quantity}," \
-            f"id_category = {new_id_category}," \
-            f"WHERE id = {id_product}"
-        self.database.execute_sql(sql(id_product, new_name, new_description,new_price, new_quantity, new_id_category))        
-        
-    def display_all_product(self):
-        sql = "SELECT product.id, product.name, product.description, product.price, product.quantity, product.id_category category.nom AS nom_category " \
-          "FROM product " \
-          "LEFT JOIN category ON product.id_category = category.id;"        
-        all_product = self.database.fetch_all(sql)
-        for row in all_product:
-            print(f"{row[0]} {row[1]} | Prix: {row[2]} | Quantité : {row[3]} | Catégory : {row[4]}")
-        return all_product       
+        sql = f"UPDATE product" \
+              f" SET " \
+              f" name = '{new_name}'," \
+              f" description = '{new_description}'," \
+              f" price = {new_price}," \
+              f" quantity = {new_quantity}," \
+              f" id_category = {new_id_category}" \
+              f" WHERE id = {id_product};"
+        self.database.execute_sql(sql)
+
+    def get_all_products(self):
+        sql = "SELECT * FROM product"
+        return self.database.fetch_all(sql)
+    
+    def fetch_all_products(self):
+        sql = "SELECT * FROM product"
+        self.database.cursor.execute(sql)
+        return self.database.cursor.fetchall()
+      
    
         
-    # def modify_price(self, id_product, new_price):
-    #     sql = f"UPDATE product" \
-    #         f"SET price = {new_price}" \
-    #             f"WHERE id = {id_product};"
-    #     self.database.request.execute(sql)
-    #     self.database.base.commit()
-        
-    # def modify_quantity(self, id_product, new_quantity):
-    #     sql = f"UPDATE product" \
-    #         f"SET quantity = {new_quantity}" \
-    #             f"WHERE id = {id_product};"
-    #     self.request.execute(sql)
-    #     self.base.commit()
+    
     
     
         
